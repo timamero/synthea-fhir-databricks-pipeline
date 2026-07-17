680b683c0dca55376a64b4c429a3ee4e091553c4
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-07-17 13:47:40 -0700

    Add doc

---
+++ api/app/service.py
@@ -0,0 +1,4 @@
+# Read data from Databricks SQL using the Databricks SQL Connector for Python
+# References:
+# - https://docs.databricks.com/aws/en/dev-tools/python-sql-connector
+# - https://docs.databricks.com/aws/en/dev-tools/auth/pat#pat-user
