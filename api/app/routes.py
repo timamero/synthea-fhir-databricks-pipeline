f0c916d7f25d61b70d464a574295e42df24faf57
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-07-17 19:40:37 -0700

    Update route to fetch condition counts

--- api/app/routes.py
+++ api/app/routes.py
@@ -1,19 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from .schema import ConditionCount
+from .service import get_condition_counts
 
 router = APIRouter(prefix="/api")
 
 
-@router.get("/condition_counts")
-def get_condition_counts():
-    # Sample data for demonstration purposes
-    sample_data = [
-        ConditionCount(
-            gender="Male", condition_description="Hypertension", condition_count=150
-        ),
-        ConditionCount(
-            gender="Female", condition_description="Diabetes", condition_count=200
-        ),
-    ]
-    return sample_data
+@router.get("/condition_counts", response_model=list[ConditionCount])
+def read_condition_counts():
+    """
+    Endpoint to fetch the count of conditions grouped by gender and condition
+    description from the Databricks SQL database.
+    Returns:
+        List[ConditionCount]: A list of ConditionCount objects containing the gender,
+        condition description, and the count of each condition.
+    """
+    condition_counts = get_condition_counts()
+
+    if condition_counts is None:
+        raise HTTPException(
+            status_code=500, detail="Error fetching condition counts from the database."
+        )
+
+    return condition_counts
