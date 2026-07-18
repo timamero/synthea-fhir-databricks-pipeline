17294d0ff918d4c9495f7aa72e8cd009e274d3e8
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-07-17 19:22:29 -0700

    Test service to fetch data from databricks

--- api/app/main.py
+++ api/app/main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 
 from .routes import router
 
+from .service import get_condition_counts
+
 load_dotenv()
 
 app = FastAPI()
@@ -18,3 +20,5 @@ app.add_middleware(
 )
 
 app.include_router(router)
+
+get_condition_counts()  # Test the function to ensure it works correctly
--- api/app/service.py
+++ api/app/service.py
@@ -2,3 +2,29 @@
 # References:
 # - https://docs.databricks.com/aws/en/dev-tools/python-sql-connector
 # - https://docs.databricks.com/aws/en/dev-tools/auth/pat#pat-user
+
+from databricks import sql
+import os
+
+
+def get_condition_counts():
+    query = """
+  SELECT p.gender, c.condition_description, COUNT(*) AS condition_count
+  FROM learning.synthea.conditions c
+  JOIN learning.synthea.patients p ON c.patient_id = p.patient_id
+  GROUP BY p.gender, c.condition_description
+  ORDER BY condition_count DESC LIMIT 5;
+  """
+
+    with sql.connect(
+        server_hostname=os.getenv("DATABRICKS_SERVER_HOSTNAME"),
+        http_path=os.getenv("DATABRICKS_HTTP_PATH"),
+        access_token=os.getenv("DATABRICKS_TOKEN"),
+    ) as connection:
+
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            result = cursor.fetchall()
+
+            for row in result:
+                print(row)
