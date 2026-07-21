ad2353af5bc9dd02bbed0a89d0b10b0752329204
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-07-20 21:43:11 -0700

    Fetch condition counts and save to state variable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,37 @@
+import { useState, useEffect } from 'react';
 import '@mantine/core/styles.css';
 
 import { MantineProvider } from '@mantine/core';
 import { Title } from '@mantine/core';
 
+import {
+  fetchConditionCounts,
+  type ConditionCountsResponse,
+} from './services/syntheaApiService';
+
 export default function App() {
+  const [conditionCounts, setConditionCounts] = useState<
+    ConditionCountsResponse[] | null
+  >(null);
+
+  const baseUrl = 'http://localhost:8080';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchConditionCounts(baseUrl);
+
+      if (data) {
+        setConditionCounts(data);
+      } else {
+        console.warn('No condition counts data received from the API.');
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log('conditionCounts:', conditionCounts);
+
   return (
     <MantineProvider>
       <div>
