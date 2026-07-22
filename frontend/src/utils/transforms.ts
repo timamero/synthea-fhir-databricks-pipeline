20cc4a6c51534e087259c77eb00cae8606a396f0
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-07-22 12:18:02 -0700

    Create util functions to transform data

---
+++ frontend/src/utils/transforms.ts
@@ -0,0 +1,34 @@
+import {
+  type ConditionCountsResponse,
+  type PivotedConditionCounts,
+} from './types';
+
+export function pivotByCondition(
+  rows: ConditionCountsResponse[],
+): PivotedConditionCounts[] {
+  const grouped: Record<string, PivotedConditionCounts> = {};
+
+  rows.forEach(({ gender, condition_description, condition_count }) => {
+    if (!grouped[condition_description]) {
+      grouped[condition_description] = { condition_description };
+    }
+
+    if (gender === 'male' || gender === 'female') {
+      grouped[condition_description][gender] = condition_count;
+
+      grouped[condition_description].male ??= 0;
+      grouped[condition_description].female ??= 0;
+    }
+  });
+
+  return Object.values(grouped);
+}
+
+export function sortPivotedConditionCountsByGender(
+  data: PivotedConditionCounts[],
+): PivotedConditionCounts[] {
+  return data.sort(
+    (a, b) =>
+      (b.male ?? 0) + (b.female ?? 0) - ((a.male ?? 0) + (a.female ?? 0)),
+  );
+}
